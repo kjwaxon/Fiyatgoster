6834c501934e84f4f06d1df49eaa0632cc7a6b6b
kjwaxon <kaanaslan1996@hotmail.com>
2022-04-19 14:24:18 +0300

    Fiyat Göster

---
+++ Fiyat/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiyat
+{
+    public abstract class Price
+    {
+        public abstract void ProductPrice();
+    }
+        public class elma : Price
+    {
+        public override void ProductPrice()
+        {
+            int elmaprice = 15;
+            Console.WriteLine("Elmanın kilosu : "+elmaprice);
+        }
+    }
+        public class kola : Price
+    {
+        public override void ProductPrice()
+        {
+            int kolaprice = 18;
+            Console.WriteLine("Kolanın fiyatı : "+kolaprice);
+        }
+    }
+}
---
+++ Fiyat/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Fiyat
+{
+    class Fiyat
+    {
+        static void Main(string[] args)
+        {
+            elma e = new elma();
+            e.ProductPrice();
+
+            kola k = new kola();
+            k.ProductPrice();
+        }
+    }
+}
